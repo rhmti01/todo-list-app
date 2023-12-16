cef1d68bc27b97b2e2a875c91ce6afd0d0847105
Abolfazl Rahmati <rahmati.business@gmail.com>
2023-12-16 18:11:55 +0330

    tailwind installation and configuration

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        inter: ['"Inter"']
+      }
+    }
+  },
+  plugins: [],
+}
+
